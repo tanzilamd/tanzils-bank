2639d4d0c8b8d58df8fa0d114ecaf90847c91f10
tanzilamd <mdtanzil858@gmail.com>
2021-05-18 15:00:51 +0600

    deposit button function added

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+// Login button event handler
+
 const loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function(){
     const loginArea = document.getElementById('login-area');
     loginArea.style.display = 'none';
     const transactionArea = document.getElementById('transaction-area');
     transactionArea.style.display = 'block';
+})
+
+// Deposit button event handler
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click', function(){
+    const depositAmount = document.getElementById('depositAmount').value; // deposit input amount
+    const depositNumber = parseFloat(depositAmount);
+    console.log(depositNumber);
+
 })
