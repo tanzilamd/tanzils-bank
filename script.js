3637c9c21239fcfc0533a093067c9d7f3495898e
tanzilamd <mdtanzil858@gmail.com>
2021-05-18 13:09:33 +0600

    submit button Javascript added

---
+++ script.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', function(){
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+})
